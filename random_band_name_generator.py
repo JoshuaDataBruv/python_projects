08e2999d087d5c0e7b7327733678798c53743c65
Joshua Caldwell <57042293+JoshuaDataBruv@users.noreply.github.com>
2024-03-26 19:08:48 -0500

    First project completed. Random band name generator.

---
+++ random_band_name_generator.py
@@ -0,0 +1,16 @@
+# Ask for favorite state and animal. That will be your band name.
+
+#What is your name?
+print('Hello ' + input('What is your name? '))
+
+#What is your favorite state?
+state = input('What is your favorite state? ')
+
+#What is your favorite animal?
+animal = input('What is your favorite animal? ')
+
+#Creating name based off of the state and animal.
+band_name = 'The ' + state + ' ' + animal
+
+#here is your band name.
+print(band_name)
